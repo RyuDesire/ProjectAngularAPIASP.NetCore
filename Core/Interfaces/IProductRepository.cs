60c43aa38825bffc704aa46b6ae4d4365602a30a
RyuDesire <cesarstiven2017@gmail.com>
2020-11-23 13:14:39 -0500

    [skinet] cm005 - New entities created, new migrations, applying migrations and creating database on start

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProducts();
+    }
+}
