83350ea22f81cde3621bc2c9ac9385ae5da07165
RyuDesire <cesarstiven2017@gmail.com>
2020-11-23 16:54:33 -0500

    [skinet] cm007 - Applying Eager Loading to products

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -7,6 +7,8 @@ namespace Core.Interfaces
     public interface IProductRepository
     {
         Task<Product> GetProductIdAsync(int id);
-        Task<IReadOnlyList<Product>> GetProducts();
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -14,14 +14,30 @@ namespace Infrastructure.Data
             _context = context;
         }
 
-        public async Task<Product> GetProductIdAsync(int id)
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.ProductBrands.ToListAsync();
         }
 
-        public async Task<IReadOnlyList<Product>> GetProducts()
+        public async Task<Product> GetProductIdAsync(int id)
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+            .Include(p => p.ProductBrand)
+            .Include(p => p.ProductType)
+            .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+            .Include(p => p.ProductBrand)
+            .Include(p => p.ProductType)
+            .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
         }
     }
 }
